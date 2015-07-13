e6b124d75aa1e880fc37d70cfaf4232a32f9cf32
Joaquín Omar Tumas <j.tumas@gmail.com>
2015-07-13 00:08:53 -0300

    commit inicial

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularStoreApp
+ * @description
+ * # angularStoreApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularStoreApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);

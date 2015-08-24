91c356664edad02c29d1b8d0b3bbeab521bb710a
LuxDie <ezequielcicala@hotmail.com>
2015-08-23 21:12:03 -0300

    Add product list edit feature. Add new states and split edit and new product controllers.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,22 +18,42 @@ angular
 		'firebase',
 		'angularStore.api'
 	])
-	.config(function ($stateProvider) {
+	.config(function ($stateProvider, $urlRouterProvider) {
 		$stateProvider
 			.state('main', {
 				url: '/',
 				templateUrl: 'views/main.html',
 				controller: 'MainCtrl'
 			})
-			.state('product', {
-				url: '/products/:id',
-				templateUrl: 'views/product.html',
-				controller: 'ProductCtrl'
-			})
 			.state('products', {
+				abstract: true,
 				url: '/products',
-				templateUrl: 'views/products.html',
-				controller: 'ProductsCtrl'
+				template: '<div ui-view></div>'
+			})
+			.state('products.list', {
+				url: '',
+				templateUrl: 'views/products.list.html',
+				controller: 'ProductsListCtrl',
+				resolve: {
+					products: api => {
+						return api.getProducts();
+					}
+				}
+			})
+			.state('products.new', {
+				url: '/new',
+				templateUrl: 'views/products.detail.html',
+				controller: 'NewProductCtrl'
+			})
+			.state('products.edit', {
+				url: '/:productId',
+				templateUrl: 'views/products.detail.html',
+				controller: 'EditProductCtrl',
+				resolve: {
+					product: function ($stateParams, api) {
+						return api.getProductsByProductId({productId: $stateParams.productId});
+					}
+				}
 			})
 			.state('user', {
 				url: '/users/:id',
@@ -50,8 +70,11 @@ angular
 				templateUrl: 'views/login.html',
 				controller: 'LoginCtrl'
 			});
+		$urlRouterProvider
+		.otherwise('/');
 	})
 	.run(($rootScope, $state, auth) => {
+
 		$rootScope.$on('$stateChangeStart', (event, toState) => {
 			// Redirect to login if the user is not authenticated
 			if (toState.name !== 'login' && !auth.$getAuth()) {
@@ -59,4 +82,11 @@ angular
 				$state.go('login');
 			}
 		});
+
+		$rootScope.$on('$stateChangeError', () => {
+			alert('There was an error loading data.');
+		});
+
+		$rootScope.$state = $state;
+
 	});
